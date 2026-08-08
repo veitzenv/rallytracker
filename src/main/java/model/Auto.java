9145f7c16a95983e563d574ec343307f753e7a3a
nicolasfernandez <alexis.fernandez@estudiantes.utec.edu.uy>
2026-08-08 00:07:10 -0300

    Creación de entidades Auto y Resultado con relación uno a muchos.

--- src/main/java/model/Auto.java
+++ src/main/java/model/Auto.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Getter @Setter
+@Entity // Le digo a Spring que esta clase es una tabla en la BD.
 public class Auto {
+    @Id // Indico el atributo que es PK.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Indico que sea autoincremental.
+    private int id;
+    private String piloto;
+    private int dorsal;
+    private int tiempoTotal;
+    @OneToMany (mappedBy = "referencia")
+    private List<Resultado> listResultados;
+
 }
---
+++ src/main/java/model/Resultado.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+
+@Getter @Setter
+@Entity
+public class Resultado {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    private Auto referencia;
+    private int tramo;
+    private int tiempo;
+    private int penalizaciones;
+}
