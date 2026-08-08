36318395a755ca4f177e7285e9b74d8556e1592b
nicolasfernandez <alexis.fernandez@estudiantes.utec.edu.uy>
2026-08-07 23:40:46 -0300

    Proyecto inicial

---
+++ src/main/java/com/veitz/rallytracker/RallytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.veitz.rallytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallytrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RallytrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veitz/rallytracker/RallytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veitz.rallytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallytrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
